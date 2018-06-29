97f644badb0dc19f38e921dc838d40f16b56770b
kaysiz <ksiziva@gmail.com>
2018-06-29 17:23:15 +0200

    added actual libft

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,78 @@
 /*   By: ksiziva <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/22 09:59:01 by ksiziva           #+#    #+#             */
-/*   Updated: 2018/06/22 09:59:05 by ksiziva          ###   ########.fr       */
+/*   Updated: 2018/06/29 17:15:29 by ksiziva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+int		get_line(char **save, char ***line)
+{
+	int		x;
+	char	*sub;
+
+	if (ft_strchr(*save, '\n'))
+	{
+		x = 0;
+		while (save[0][x] != '\n')
+			x++;
+		**line = ft_strcat(**line, ft_strsub(*save, 0, x));
+		sub = ft_strchr(*save, '\n');
+		*save = ft_strdup(sub) + 1;
+		return (1);
+	}
+	else
+	{
+		**line = ft_strcat(**line, *save);
+	}
+	return (0);
+}
+
+int		read_text(int fd, char **line)
+{
+	char			buff[BUFF_SIZE + 1];
+	static	char	*save = "";
+	char			*sub;
+	int				x;
+	int				ret;
+
+	if (get_line(&save, &line) == 1)
+		return (1);
+	while ((ret = read(fd, buff, BUFF_SIZE)))
+	{
+		buff[ret] = '\0';
+		if (ft_strchr(buff, '\n'))
+		{
+			x = 0;
+			while (buff[x] != '\n')
+				x++;
+			*line = ft_strcat(*line, ft_strsub(buff, 0, x));
+			sub = ft_strchr(buff, '\n');
+			save = ft_strdup(sub) + 1;
+			return (1);
+		}
+		else
+			*line = ft_strcat(*line, buff);
+	}
+	return (0);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	int		x;
+	char	*str;
+
+	str = "";
+	if (str)
+		ft_bzero(str, ft_strlen(str));
+	if (fd < 0)
+		return (-1);
+	x = read_text(fd, &str);
+	*line = str;
+	if (ft_strlen(str) > 0)
+		return (1);
+	if (x == 0)
+		free(*line);
+	return (x);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,18 @@
 /*   By: ksiziva <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/22 09:59:24 by ksiziva           #+#    #+#             */
-/*   Updated: 2018/06/22 09:59:26 by ksiziva          ###   ########.fr       */
+/*   Updated: 2018/06/29 17:17:15 by ksiziva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <unistd.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+
+#endif
